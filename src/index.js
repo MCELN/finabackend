a3c9c38f4ed7fd63913e6c70e5d2b289a7551310
Cristian Lanza <cristianlanza@gmail.com>
2023-12-31 16:57:31 -0300

    entrega

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const app = require('./server');
 const { port } = require('./config');
 const realTimeServer = require('./realTimeServer');
+const PORT = port || process.env.PORT || 8080
 
-const httpServer = app.listen(port, () => {
-    console.log(`Server running at port ${port}`)
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
 })
 
 realTimeServer(httpServer);
